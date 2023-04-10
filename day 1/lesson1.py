9d3185430523e88d7d8eda78421cfa3861d796cd
unknown <qdragunov@gmail.com>
2023-04-10 22:03:55 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,44 @@
+from turtle import *
+
+#we want to paint a house
+
+#step 1 draw a square
+
+width(9)
+color("red")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+#end of  square
+
+#drawing a door
+
+forward(70)
+left(90)
+forward(120)
+right(90)
+forward(60)
+
+
+right(90)
+forward(60)
+
+
+
+
+
+
+
+
+
+exitonclick
+
